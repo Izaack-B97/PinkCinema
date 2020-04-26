d0a00cf93c1143b4333bca7aa868232a92afc8bb
Isaac Bustamante <izaack.97@gmail.com>
2020-04-25 23:49:20 -0700

    Comentamos codigo inecesario

--- private/query_functions.php
+++ private/query_functions.php
@@ -86,89 +86,89 @@ function delete_pelicula($id){
 
 /** END MIS FUNCIONES ****/
 
-function find_all_subjects(){
+// function find_all_subjects(){
 
-	global $db;
+// 	global $db;
 
-	$sql = "SELECT * FROM peliculas ";
-	//$sql .= "ORDER BY position ASC";
-	//echo $sql;
-	$result = mysqli_query($db, $sql);
-	confirm_result_set($result);
-	return $result;
-}
+// 	$sql = "SELECT * FROM peliculas ";
+// 	//$sql .= "ORDER BY position ASC";
+// 	//echo $sql;
+// 	$result = mysqli_query($db, $sql);
+// 	confirm_result_set($result);
+// 	return $result;
+// }
 
-function find_subject_by_id($id){
-	global $db;
+// function find_subject_by_id($id){
+// 	global $db;
 
-	$sql = "SELECT * FROM peliculas ";
-	$sql .= "WHERE id='" . $id . "'";
-	$result = mysqli_query($db, $sql);
-	confirm_result_set($result);
-	$subject = mysqli_fetch_assoc($result);
-	mysqli_free_result($result);
-	return $subject; //return an assoc. array
+// 	$sql = "SELECT * FROM peliculas ";
+// 	$sql .= "WHERE id='" . $id . "'";
+// 	$result = mysqli_query($db, $sql);
+// 	confirm_result_set($result);
+// 	$subject = mysqli_fetch_assoc($result);
+// 	mysqli_free_result($result);
+// 	return $subject; //return an assoc. array
 
-}
+// }
 
-function has_unique_page_menu_name($nombre) {
-    global $db;
-    $sql = "SELECT * FROM peliculas ";
-    $sql .= "WHERE nombre='" . $nombre . "' ";
-    $page_set = mysqli_query($db, $sql);
-    $page_count = mysqli_num_rows($page_set);
-    mysqli_free_result($page_set);
-    if ($page_count === 0) {
-      return true;
-    }
-}
+// function has_unique_page_menu_name($nombre) {
+//     global $db;
+//     $sql = "SELECT * FROM peliculas ";
+//     $sql .= "WHERE nombre='" . $nombre . "' ";
+//     $page_set = mysqli_query($db, $sql);
+//     $page_count = mysqli_num_rows($page_set);
+//     mysqli_free_result($page_set);
+//     if ($page_count === 0) {
+//       return true;
+//     }
+// }
 
 
-function find_all_pages(){
-	global $db;
+// function find_all_pages(){
+// 	global $db;
 
-	$sql = "SELECT * FROM pages ";
-	$sql .= "ORDER BY subject_id ASC, position ASC";
-	$result = mysqli_query($db, $sql);
-	confirm_result_set($result);
-	return $result;
-}
+// 	$sql = "SELECT * FROM pages ";
+// 	$sql .= "ORDER BY subject_id ASC, position ASC";
+// 	$result = mysqli_query($db, $sql);
+// 	confirm_result_set($result);
+// 	return $result;
+// }
 
-function insert_pelicula($pelicula){
-	global $db;
+// function insert_pelicula($pelicula){
+// 	global $db;
 
-  $sql = "INSERT INTO peliculas ";
-  $sql .= "(nombre, genero, idioma, duracion, clasificacion, funcion, imagen) ";
-  $sql .= "VALUES (";
-  $sql .= "'" . $pelicula['nombre'] . "',";
-  $sql .= "'" . $pelicula['genero'] . "',";
-  $sql .= "'" . $pelicula['idioma'] . "',";
-  $sql .= "'" . $pelicula['duracion'] . "',";
-  $sql .= "'" . $pelicula['clasificacion'] . "',";
-  $sql .= "'" . $pelicula['funcion'] . "',";
-  $sql .= "'" . $pelicula['imagen'] . "' ";
-  $sql .= ")";
+//   $sql = "INSERT INTO peliculas ";
+//   $sql .= "(nombre, genero, idioma, duracion, clasificacion, funcion, imagen) ";
+//   $sql .= "VALUES (";
+//   $sql .= "'" . $pelicula['nombre'] . "',";
+//   $sql .= "'" . $pelicula['genero'] . "',";
+//   $sql .= "'" . $pelicula['idioma'] . "',";
+//   $sql .= "'" . $pelicula['duracion'] . "',";
+//   $sql .= "'" . $pelicula['clasificacion'] . "',";
+//   $sql .= "'" . $pelicula['funcion'] . "',";
+//   $sql .= "'" . $pelicula['imagen'] . "' ";
+//   $sql .= ")";
 
-  $result = mysqli_query($db, $sql);
-  //for INSERT statements, $result is true/false
-  if ($result) {
-  	return true;
+//   $result = mysqli_query($db, $sql);
+//   //for INSERT statements, $result is true/false
+//   if ($result) {
+//   	return true;
   	
-  }else{
-  	// INSERT failed
-  	echo mysqli_error($db);
-  	db_disconnect($db);
-    	exit;
-  }
+//   }else{
+//   	// INSERT failed
+//   	echo mysqli_error($db);
+//   	db_disconnect($db);
+//     	exit;
+//   }
 
-}
+// }
 
-function update_subject($subject, $campo) {
+// function update_subject($subject, $campo) {
 
-	global $db;
+// 	global $db;
 
 
- $sql = "UPDATE peliculas SET " .$campo ."='" . $subject['campo'] . "'" . " WHERE id='" .$subject['id'] ."'" ."LIMIT 1";
+//  $sql = "UPDATE peliculas SET " .$campo ."='" . $subject['campo'] . "'" . " WHERE id='" .$subject['id'] ."'" ."LIMIT 1";
 
 
 
@@ -176,71 +176,71 @@ function update_subject($subject, $campo) {
 
     
 
-$result = mysqli_query($db, $sql);
-//for UPDATE statements, $result is true/false
+// $result = mysqli_query($db, $sql);
+// //for UPDATE statements, $result is true/false
 
-if ($result) {
-  return true;
-}else{
-  echo mysqli_error($db);
-  db_disconnect($db);
-  exit;
-}
+// if ($result) {
+//   return true;
+// }else{
+//   echo mysqli_error($db);
+//   db_disconnect($db);
+//   exit;
+// }
 
-}
-
-
-
-function update_subject2($subject) {
-
-  global $db;
+// }
 
 
-$sql = "UPDATE peliculas SET ";
-$sql .= "nombre='" . $subject['nombre'] . "',";
-$sql .= "genero='" . $subject['genero'] . "', ";
-$sql .= "idioma='" . $subject['idioma'] . "',";
-$sql .= "duracion='" . $subject['duracion'] . "', ";
-$sql .= "clasificacion='" . $subject['clasificacion'] . "',";
-$sql .= "funcion='" . $subject['funcion'] . "',";
-$sql .= "imagen='" . $subject['imagen'] . "' ";
-$sql .= "WHERE id='" . $subject['id'] . "' ";
-$sql .= "LIMIT 1";
 
-$result = mysqli_query($db, $sql);
-//for UPDATE statements, $result is true/false
+// function update_subject2($subject) {
 
-if ($result) {
-  return true;
-}else{
-  echo mysqli_error($db);
-  db_disconnect($db);
-  exit;
-}
+//   global $db;
 
-}
 
-function delete_subject($id){
-global $db;
+// $sql = "UPDATE peliculas SET ";
+// $sql .= "nombre='" . $subject['nombre'] . "',";
+// $sql .= "genero='" . $subject['genero'] . "', ";
+// $sql .= "idioma='" . $subject['idioma'] . "',";
+// $sql .= "duracion='" . $subject['duracion'] . "', ";
+// $sql .= "clasificacion='" . $subject['clasificacion'] . "',";
+// $sql .= "funcion='" . $subject['funcion'] . "',";
+// $sql .= "imagen='" . $subject['imagen'] . "' ";
+// $sql .= "WHERE id='" . $subject['id'] . "' ";
+// $sql .= "LIMIT 1";
 
-$sql = "DELETE FROM peliculas ";
-  $sql .= "WHERE id= '" . $id . "' ";
-  $sql .= "LIMIT 1";
+// $result = mysqli_query($db, $sql);
+// //for UPDATE statements, $result is true/false
 
-  $result = mysqli_query($db, $sql);
+// if ($result) {
+//   return true;
+// }else{
+//   echo mysqli_error($db);
+//   db_disconnect($db);
+//   exit;
+// }
 
-  //for DELETE statements, $result is true/false
+// }
 
-  if ($result) {
-      redirect_to(url_for('/web/mostrar/index.php'));
-    }else{
-      //DELETE failed
-      echo mysqli_error($db);
-      db_disconnect($db);
-      exit;
+// function delete_subject($id){
+// global $db;
 
-    } 
+// $sql = "DELETE FROM peliculas ";
+//   $sql .= "WHERE id= '" . $id . "' ";
+//   $sql .= "LIMIT 1";
 
-}
+//   $result = mysqli_query($db, $sql);
+
+//   //for DELETE statements, $result is true/false
+
+//   if ($result) {
+//       redirect_to(url_for('/web/mostrar/index.php'));
+//     }else{
+//       //DELETE failed
+//       echo mysqli_error($db);
+//       db_disconnect($db);
+//       exit;
+
+//     } 
+
+// }
 
 ?>
